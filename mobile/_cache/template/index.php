e91d8cc27fc9a4ae1a3a7a0dd657d49f22dab867
夏申丽 <459433186@qq.com>
2018-04-23 21:15:57 +0800

    image
    imagepuls

--- mobile/_cache/template/index.php
+++ mobile/_cache/template/index.php
@@ -5,5 +5,10 @@
 <meta name='viewport' content='width=375,user-scalable=no'>
 <?php _includeCSS("css/style.css"); ?>
 <?php _includeCSS("css/index.css"); ?>
-<title>主页</title>
-<div class="head"></div>
+<title>首页</title>
+<div class="head">
+	<div class="title">Cooking</div>
+</div>
+<div style="width: 100%;height: 30%;padding-top: 1%;"><a href="everydayrecommend.php"><img style="width: 100%;height: 100%;" src="../image/index/everydayrecommend.png"></a></div>
+<div style="width: 100%;height: 30%;padding-top: 1%;"><a href="interestingmenu.php"><img style="width: 100%;height: 100%;" src="../image/index/interestingmenu.png"></a></div>
+<div style="width: 100%;height: 30%;padding-top: 1%;"><a href="cookingdiary.php"><img style="width: 100%;height: 100%;" src="../image/index/cookingdiary.png"></a></div>
--- mobile/index.php
+++ mobile/index.php
@@ -5,5 +5,10 @@ include_once '../include/template.php';
 {viewport375}
 {css/style.css}
 {css/index.css}
-<title>主页</title>
-<div class="head"></div>
+<title>首页</title>
+<div class="head">
+	<div class="title">Cooking</div>
+</div>
+<div style="width: 100%;height: 30%;padding-top: 1%;"><a href="everydayrecommend.php"><img style="width: 100%;height: 100%;" src="../image/index/everydayrecommend.png"></a></div>
+<div style="width: 100%;height: 30%;padding-top: 1%;"><a href="interestingmenu.php"><img style="width: 100%;height: 100%;" src="../image/index/interestingmenu.png"></a></div>
+<div style="width: 100%;height: 30%;padding-top: 1%;"><a href="cookingdiary.php"><img style="width: 100%;height: 100%;" src="../image/index/cookingdiary.png"></a></div>
