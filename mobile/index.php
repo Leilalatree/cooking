dfd1dec07535d4388f01a6fa760e51159905ac13
夏申丽 <459433186@qq.com>
2018-04-23 21:31:59 +0800

    index

--- mobile/index.php
+++ mobile/index.php
@@ -9,6 +9,14 @@ include_once '../include/template.php';
 <div class="head">
 	<div class="title">Cooking</div>
 </div>
-<div style="width: 100%;height: 30%;padding-top: 1%;"><a href="everydayrecommend.php"><img style="width: 100%;height: 100%;" src="../image/index/everydayrecommend.png"></a></div>
-<div style="width: 100%;height: 30%;padding-top: 1%;"><a href="interestingmenu.php"><img style="width: 100%;height: 100%;" src="../image/index/interestingmenu.png"></a></div>
-<div style="width: 100%;height: 30%;padding-top: 1%;"><a href="cookingdiary.php"><img style="width: 100%;height: 100%;" src="../image/index/cookingdiary.png"></a></div>
+<div class="btn" onclick="location.href='everydayrecommend.php'">
+		<img style="width: 100%;height: 100%;" src="../image/index/everydayrecommend.png">
+			
+	</div>
+<div class="btn" onclick="location.href='interestingmenu.php'">
+		<img style="width: 100%;height: 100%;" src="../image/index/interestingmenu.png">
+		</div>
+<div class="btn" onclick="location.href='cookingdiary.php'">
+	<img style="width: 100%;height: 100%;" src="../image/index/cookingdiary.png">
+		
+	</div>
