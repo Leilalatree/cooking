197baed94000cb07274e21d0e9e13568147bb45c
夏申丽 <459433186@qq.com>
2018-05-06 23:55:21 +0800

    new

---
+++ mobile/_cache/template/index.php
@@ -0,0 +1,29 @@
+<?php
+
+
+?>
+<meta name='viewport' content='width=375,user-scalable=no'>
+<?php _includeCSS("css/style.css"); ?>
+<?php _includeCSS("css/index.css"); ?>
+<title>首页</title>
+<div class="head">
+	<div class="title">Cooking</div>
+	<!--<div class="backbtn" onclick="location.href='index.php'" >
+		<img style="width: 70%;height: 70%;" src="images/interestingmenu/back_btn.png">
+	</div>-->
+	<div class="rightbtn" onclick="location.href='personal.php'">
+		<img style="width: 70%;height: 70%;" src="images/index/personal_btn.png">
+	</div>
+</div>
+<div class="btn" onclick="location.href='../mrtj-h5/news.html'">
+		<img style="width: 100%;height: 100%;" src="images/index/everydayrecommend.png">			
+	</div>
+	
+<div class="btn" onclick="location.href='interestingmenu.php'">
+	<img style="width: 100%;height: 100%;" src="images/index/interestingmenu.png">
+	</div>
+<div class="btn" onclick="location.href='cookingdiary.php'">
+	<img style="width: 100%;height: 100%;" src="images/index/cookingdiary.png">	
+	</div>
+
+
